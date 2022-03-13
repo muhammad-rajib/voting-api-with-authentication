0d15c550c149e2be0032aaa0e63be213305d6115
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-03-13 23:40:41 +0600

    CI/CD
    - added test file
    - added yml file for deployment

--- test/test_users.py
+++ test/test_users.py
@@ -1,6 +1,35 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from app import schemas 
+from app import schemas
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# database_url = 'postgresql://username:password@hostname:port/database-name'
+database_url = (f"postgresql://"
+                f"{settings.database_username}"
+                f":{settings.database_password}"
+                f"@{settings.database_hostname}"
+                f":{settings.database_port}"
+                f"/{settings.database_name}")
+
+
+engine = create_engine(database_url)
+
+testing_session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def override_get_db():
+    db = testing_session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 client = TestClient(app)
 
@@ -14,3 +43,4 @@ def test_create_user():
     new_user = schemas.CreateUserResponse(**res.json())
     assert new_user.email == "hello2@gmail.com"
     assert res.status_code == 201
+
